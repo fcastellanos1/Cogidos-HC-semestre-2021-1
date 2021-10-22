d2149c91db933df47a2c92c89a4bab6ab8c8f4b4
Fabio Castellanos <fcastellanos@unal.edu.co>
2021-10-22 13:08:32 -0500

    Mi primer makefile

---
+++ 2021-10-22-MakeFiles/suma.cpp
@@ -0,0 +1,39 @@
+//Este programa compara la suma armonica realizada de dos formas diferentes para mostrar las diferencias computacionales.
+
+#include<iostream>
+#include<cmath>
+
+typedef float REAL;
+REAL sumup(int N);
+REAL sumdown(int N);
+
+int main(void)
+{
+  std::cout.precision(6);
+  std::cout.setf(std::ios::scientific);
+
+  for(int ii=1;ii<100;++ii){
+    REAL suma1 = sumup(ii);
+    REAL suma2 = sumdown(ii);
+    std::cout << ii << "\t" << std::fabs(1 - suma1/suma2) << "\n";
+  }
+  return 0;
+}
+
+REAL sumup(int N)
+{
+  REAL suma = 0;
+  for(int ii = 1; ii<=N; ii++){
+    suma += 1.0/ii;
+  }
+  return suma;
+}
+
+REAL sumdown(int N)
+{
+  REAL suma = 0;
+  for(int ii = N; ii>=1; ii--){
+    suma += 1.0/ii;
+  }
+  return suma;
+}
